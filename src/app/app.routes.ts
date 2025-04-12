69967e13e9352cf467981fa97e75c369b25f6159
younes-oss <aitchattouyounes@gmail.com>
2025-04-12 19:25:23 +0200

    la creation du composant projets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ProposComponent } from './propos/propos.component';
+import { ProjetsComponent } from './projets/projets.component';
 
 
 
 export const routes: Routes = [
     {path:'accueil',component:AccueilComponent},
     {path:'propos',component:ProposComponent},
+    {path:'projets',component:ProjetsComponent}
 ];
 
